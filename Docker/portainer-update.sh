68505a4663af8e322fc6c224957765303f726cb5
Maxim Voynov <virus14m@gmail.com>
2021-12-28 22:59:43 +0700

    Create portainer-update.sh

---
+++ Docker/portainer-update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker stop portainer
+docker rm portainer
+docker rmi portainer/portainer-ce
+docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
